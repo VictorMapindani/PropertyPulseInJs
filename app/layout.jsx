4103909fd53f02536ff0b9aaeb7ee7a43be6c978
Victor Mapindani <victormapindani193@gmail.com>
2024-09-26 05:26:04 +0200

    initial-commit

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+
+//entry point for next.js
+
+import '@/assets/styles/globals.css';
+
+const MainLayout = ({children}) => {
+    return ( 
+        <html>
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+     );
+}
+ 
+export default MainLayout;
